541fc92e186f549ef9865644aad02ee7524e30e1
kylegibson15 <kylegibson15@gmail.com>
2018-01-24 16:13:13 -0700

    signup input field, not connected to db yet

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Image, KeyboardAvoidingView, } from 'react-native';
+import SignUpForm from './SignUpForm';
+
+export default class SignUp extends Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Text style={styles.title}>HealthAlarm</Text>
+          <Image style={styles.clock} source={clock}/>
+        </View>
+        <View style={styles.signUpForm}>
+          <SignUpForm navigation={this.props.navigation}/>
+        </View>
+      </KeyboardAvoidingView>
+    )
+  }
+}
+
+var clock = require('../../assets/images/alarm-clock.png');
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2c3e50',
+  },
+  logoContainer: {
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+  clock: {
+    height: 60,
+    width: 60,
+  },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  signUpForm: {
+    // marginTop: 90,
+  }
+})
