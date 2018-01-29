561e446c65e9867916fd2edbd53107a7783981a7
kylegibson15 <kylegibson15@gmail.com>
2018-01-29 09:57:56 -0700

    java build for backgroundtimer and APIAI

--- android/app/src/main/java/com/healthalarm/MainApplication.java
+++ android/app/src/main/java/com/healthalarm/MainApplication.java
@@ -3,6 +3,8 @@ package com.healthalarm;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.ocetnik.timer.BackgroundTimerPackage;
+import de.innfactory.apiai.RNApiAiPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new BackgroundTimerPackage(),
+            new RNApiAiPackage()
       );
     }
 
