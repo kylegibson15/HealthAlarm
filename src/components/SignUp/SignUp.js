987d421cfe885217240069cd9d1d549da9b076cf
kylegibson15 <kylegibson15@gmail.com>
2018-02-16 10:25:58 -0700

    clean code and correct indentation

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text, Image, KeyboardAvoidingView, } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, Image, KeyboardAvoidingView} from 'react-native';
 import SignUpForm from './SignUpForm';
 
 export default class SignUp extends Component {
   render() {
-    return (
-      <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <View style={styles.logoContainer}>
-          <Text style={styles.title}>HealthAlarm</Text>
-          <Image style={styles.clock} source={clock}/>
-        </View>
-        <View style={styles.signUpForm}>
-          <SignUpForm state={this.props} newUserInfo={this.props.newUserInfo} />
-        </View>
-      </KeyboardAvoidingView>
-    )
+    return (<KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Text style={styles.title}>HealthAlarm</Text>
+        <Image style={styles.clock} source={clock}/>
+      </View>
+      <View style={styles.signUpForm}>
+        <SignUpForm state={this.props} newUserInfo={this.props.newUserInfo}/>
+      </View>
+    </KeyboardAvoidingView>)
   }
 }
 
@@ -22,25 +20,22 @@ var clock = require('../../assets/images/alarm-clock.png');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2c3e50',
+    backgroundColor: '#2c3e50'
   },
   logoContainer: {
     justifyContent: 'center',
     flexDirection: 'column',
     alignItems: 'center',
-    flexGrow: 1,
+    flexGrow: 1
   },
   clock: {
     height: 60,
-    width: 60,
+    width: 60
   },
   title: {
     color: 'white',
     fontSize: 30,
     fontWeight: 'bold',
-    padding: 10,
-  },
-  signUpForm: {
-    // marginTop: 90,
+    padding: 10
   }
 })
--- src/components/SignUp/SignUpForm.js
+++ src/components/SignUp/SignUpForm.js
@@ -6,92 +6,38 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
-  StatusBar,
+  StatusBar
 } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import {Actions} from 'react-native-router-flux';
 
 export default class SignUpForm extends Component {
 
-  giveMeLogs(){
+  giveMeLogs() {
     let data = {
       first_name: this.firstNameInputValue,
       last_name: this.lastNameInputValue,
       email: this.emailInputValue,
       username: this.usernameInputValue,
-      password: this.passwordInputValue,
+      password: this.passwordInputValue
     }
     this.props.newUserInfo(data)
     Actions.login()
   }
 
   render() {
-    console.log('line 22 SIGN UP FORM:', this.props);
     return (<View style={styles.container}>
-      <StatusBar
-      barStyle="dark-content"
-    />
-      <TextInput
-        name="first_name"
-        placeholder="first name"
-        placeholderTextColor="white"
-        returnKeyType="next"
-        ref={(input) => this.firstNameInput = input}
-        onChangeText={(first_name) => this.firstNameInputValue = first_name}
-        onSubmitEditing={() => this.lastNameInput.focus()}
-        autoCapitolize={false}
-        style={styles.input}
-      />
+      <StatusBar barStyle="dark-content"/>
+      <TextInput name="first_name" placeholder="first name" placeholderTextColor="white" returnKeyType="next" ref={(input) => this.firstNameInput = input} onChangeText={(first_name) => this.firstNameInputValue = first_name} onSubmitEditing={() => this.lastNameInput.focus()} autoCapitolize={false} style={styles.input}/>
 
-      <TextInput
-        name="last_name"
-        placeholder="last name"
-        placeholderTextColor="white"
-        returnKeyType="next"
-        ref={(input) => this.lastNameInput = input}
-        onChangeText={(last_name) => this.lastNameInputValue = last_name}
-        onSubmitEditing={() => this.emailInput.focus()}
-        autoCapitolize={false}
-        style={styles.input}
-      />
+      <TextInput name="last_name" placeholder="last name" placeholderTextColor="white" returnKeyType="next" ref={(input) => this.lastNameInput = input} onChangeText={(last_name) => this.lastNameInputValue = last_name} onSubmitEditing={() => this.emailInput.focus()} autoCapitolize={false} style={styles.input}/>
 
-      <TextInput
-        name="email"
-        placeholder="email"
-        placeholderTextColor="white"
-        returnKeyType="next"
-        keyboardType="email-address"
-        ref={(input) => this.emailInput = input}
-        onChangeText={(email) => this.emailInputValue = email}
-        onSubmitEditing={() => this.usernameInput.focus()}
-        autoCapitolize={false}
-        style={styles.input}
-      />
+      <TextInput name="email" placeholder="email" placeholderTextColor="white" returnKeyType="next" keyboardType="email-address" ref={(input) => this.emailInput = input} onChangeText={(email) => this.emailInputValue = email} onSubmitEditing={() => this.usernameInput.focus()} autoCapitolize={false} style={styles.input}/>
 
-      <TextInput
-        name="username"
-        placeholder="username"
-        placeholderTextColor="white"
-        returnKeyType="next"
-        ref={(input) => this.usernameInput = input}
-        onChangeText={(username) => this.usernameInputValue = username}
-        onSubmitEditing={() => this.passwordInput.focus()}
-        autoCapitolize={false}
-        style={styles.input}
-      />
+      <TextInput name="username" placeholder="username" placeholderTextColor="white" returnKeyType="next" ref={(input) => this.usernameInput = input} onChangeText={(username) => this.usernameInputValue = username} onSubmitEditing={() => this.passwordInput.focus()} autoCapitolize={false} style={styles.input}/>
 
-      <TextInput
-        name="password"
-        secureTextEntry
-        placeholder="password"
-        placeholderTextColor="white"
-        returnKeyType="go"
-        onChangeText={(password) => this.passwordInputValue = password}
-        ref={(input) => this.passwordInput = input}
-        autoCapitolize={false}
-        style={styles.input}
-      />
+      <TextInput name="password" secureTextEntry="secureTextEntry" placeholder="password" placeholderTextColor="white" returnKeyType="go" onChangeText={(password) => this.passwordInputValue = password} ref={(input) => this.passwordInput = input} autoCapitolize={false} style={styles.input}/>
 
-      <TouchableOpacity onPress = {() => this.giveMeLogs()} style={styles.buttonContiainer}>
+      <TouchableOpacity onPress="onPress" {() => this.giveMeLogs()} style={styles.buttonContiainer}>
         <Text style={styles.buttonText}>
           Sign Up
         </Text>
@@ -103,22 +49,22 @@ export default class SignUpForm extends Component {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
-    backgroundColor: '#f39c12',
+    backgroundColor: '#f39c12'
   },
   input: {
     height: 40,
     backgroundColor: 'rgba(255,255,255, 0.2)',
     marginBottom: 10,
     color: '#FFF',
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
-  buttonContiainer:{
+  buttonContiainer: {
     backgroundColor: '#2c3e50',
     paddingVertical: 15
   },
   buttonText: {
     textAlign: 'center',
     color: '#FFF',
-    fontWeight: '700',
+    fontWeight: '700'
   }
 })
